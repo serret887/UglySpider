8d549f65b78f82fa2ea9f45332edce4976831335
Ale Serret <ASerret@plymouthenergy.com>
2017-02-23 17:13:17 -0500

    Creating Cookies system missing sessions.

--- pitcher/pitcher.go
+++ pitcher/pitcher.go
@@ -1,7 +1,6 @@
 package pitcher
 
 import (
-	"log"
 	"net/http"
 	"net/http/cookiejar"
 	"net/url"
@@ -10,13 +9,35 @@ import (
 	"golang.org/x/net/publicsuffix"
 )
 
-// NewProxySpider create a new worker that proxy all their
-// request over a proxy specifyed in the pAddress parameter
-func NewProxySpider(pAddress string) (*http.Client, error) {
+type Pitcher struct {
+	pURL *url.URL
+	*http.Client
+}
+
+// NewPitcher is the constructor of a new Pictcher the parameter
+// passed to the constructor is the
+func NewPitcher(pAddress string) (*Pitcher, error) {
+	// if pAddress == "" {
+	// 	client, err := newSimpleSpider()
+	// 	if err != nil {
+	// 		return nil, err
+	// 	}
+	// 	return &Pitcher{Client: client}, nil
+	// }
 	pURL, err := url.Parse(pAddress)
 	if err != nil {
 		return nil, err
 	}
+	client, err := NewProxySpider(pURL)
+	if err != nil {
+		return nil, err
+	}
+	return &Pitcher{pURL: pURL, Client: client}, nil
+}
+
+// NewProxySpider create a new worker that proxy all their
+// request over a proxy specifyed in the pAddress parameter
+func NewProxySpider(pURL *url.URL) (*http.Client, error) {
 
 	// setting up the transport
 	ptransport := &http.Transport{
@@ -24,15 +45,22 @@ func NewProxySpider(pAddress string) (*http.Client, error) {
 		Proxy:           http.ProxyURL(pURL),
 	}
 	//setting up COOKIES
+	client, err := newSimpleSpider()
+	if err != nil {
+		return nil, err
+	}
+	client.Transport = ptransport
+	// Creating the Client
+	return client, nil
+}
+func newSimpleSpider() (*http.Client, error) {
+	//setting up COOKIES
 	jar, err := cookiejar.New(&cookiejar.Options{PublicSuffixList: publicsuffix.List})
 	if err != nil {
-		log.Fatal(err)
+		return nil, err
 	}
-	client := &http.Client{
-		Jar:       jar,
-		Transport: ptransport,
-	}
+	return &http.Client{
+		Jar: jar,
+	}, nil
 
-	// Creating the Worker
-	return client, nil
 }
--- pitcher/pitcher_test.go
+++ pitcher/pitcher_test.go
@@ -1,12 +1,69 @@
 package pitcher_test
 
 import (
-	. "github.com/serret887/UglySpider/pitcher"
+	"fmt"
+	"net/http"
+	"net/http/httptest"
 
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
+	"github.com/serret887/UglySpider/pitcher"
 )
 
 var _ = Describe("Pitcher", func() {
+	Context("making HTTP request", func() {
+		var fakeServer *httptest.Server
+		BeforeEach(func() {
+			fakeServer = httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+				cookie, err := r.Cookie("times")
+				if err != nil {
+					cookie = &http.Cookie{Name: "times"}
+				}
+				fmt.Print("Request executed")
+				cookie.Value += "1"
+				http.SetCookie(w, cookie)
+				fmt.Fprintln(w, "Hello, client")
+			}))
+		})
 
+		It("Will get the correct server response", func() {
+
+			p, err := pitcher.NewPitcher(fakeServer.URL)
+			Expect(err).To(BeNil(), "Should be able to create a Pitcher")
+			res, err := p.Get(fakeServer.URL)
+			Expect(err).To(BeNil(), "Should make a get request without problems")
+			defer res.Body.Close()
+			Expect(res).NotTo(BeEquivalentTo("Hello Client"), "the response should keep the cookies")
+		})
+		It("Will save the cookies passed by the server", func() {
+
+			p, err := pitcher.NewPitcher(fakeServer.URL)
+			Expect(err).To(BeNil(), "Should be able to create a Pitcher")
+			res, err := p.Get(fakeServer.URL)
+			Expect(err).To(BeNil(), "Should make a get request without problems")
+			defer res.Body.Close()
+			cookies := res.Cookies()
+			Expect(cookies[0].Name).To(Equal("times"), "the name of the cookie should be times")
+			Expect(cookies[0].Value).To(Equal("1"), "the name of the cookie should be times")
+
+		})
+		It("Will save the cookies passed by the server for all the request", func() {
+
+			p, err := pitcher.NewPitcher(fakeServer.URL)
+			Expect(err).To(BeNil(), "Should be able to create a Pitcher")
+			res, err := p.Get(fakeServer.URL)
+			res, err = p.Get(fakeServer.URL)
+			res, err = p.Get(fakeServer.URL)
+			Expect(err).To(BeNil(), "Should make a get request without problems")
+			defer res.Body.Close()
+			cookies := res.Cookies()
+			Expect(cookies[0].Name).To(Equal("times"), "the name of the cookie should be times")
+			Expect(cookies[0].Value).To(Equal("111"), "the name of the cookie should be times")
+
+		})
+
+		AfterEach(func() {
+			fakeServer.Close()
+		})
+	})
 })
